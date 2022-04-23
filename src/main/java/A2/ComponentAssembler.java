e2e78c2596be7debcf2ef49aac137c9eb4b7589e
Siu Cheng <herrcheng@gmail.com>
2022-04-23 23:04:19 +0200

    refactor code

--- src/main/java/A2/ComponentAssembler.java
+++ src/main/java/A2/ComponentAssembler.java
@@ -10,16 +10,34 @@ import java.net.URLClassLoader;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ComponentAssembler {
 
-    static final String pathResources = "src/main/java/A2/resources/";
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String pathResources = "src/main/java/A2/resources/";
     private final Scanner sc;
 
     public ComponentAssembler() {
         sc = new Scanner(System.in);
+    }
+
+    public void run() throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        System.out.println("Start Component Assembler");
+        while (true) {
+            System.out.println("------------------------------------------");
+            String[] options = {
+                    "show status",
+                    "run component",
+                    "stop component"
+            };
+            String input = ask("Please select number:", options, Breaker.EXIT);
+            if (input.equals((options.length - 1) + "") || input.equals(options[options.length - 1])) {
+                break;
+            } else if (input.equals("1")) {
+                loadJar();
+            }
+        }
     }
 
 /*    static Object neuesExemplar(String pfad, String klassename) throws Exception {
@@ -40,21 +58,27 @@ public class ComponentAssembler {
         return c.newInstance();
     }*/
 
+/*    static <T> T[] append(T[] arr, T element) {
+        final int n = arr.length;
+        arr = Arrays.copyOf(arr, n + 1);
+        arr[n] = element;
+        return arr;
+    }*/
+
     private void loadJar() throws IOException, ClassNotFoundException, IllegalAccessException, InvocationTargetException {
 
         // SELECT COMPONENT
-        String[] components = listJarFiles(pathResources).toArray(new String[0]);
-        String selectedComponent = ask("Please select a component:", components);
+        String[] components = getJarFiles(pathResources);
+        String selectedComponent = ask("Please select a component:", components, Breaker.EXIT);
 
         // SELECT CLASS
-        String[] classes = getClassNamesFromJarFile(pathResources + "/" + components[Integer.parseInt(selectedComponent)])
-                .toArray(new String[0]);
-        String selectedClass = ask("Please select a class:", classes);
+        String[] classes = getClassNamesFromJarFile(pathResources + "/" + components[Integer.parseInt(selectedComponent)]);
+        String selectedClass = ask("Please select a class:", classes, Breaker.EXIT);
 
         // SELECT METHOD
         String pathComponent = pathResources + "/" + selectedComponent;
         String[] methods = getMethods(classes, selectedClass, pathResources + "/" + selectedComponent);
-        String selectedMethod = ask("Please select method:", methods);
+        String selectedMethod = ask("Please select method:", methods, Breaker.EXIT);
 
         // RUN METHOD
         runMethod(classes, selectedClass, pathComponent, selectedMethod);
@@ -71,22 +95,22 @@ public class ComponentAssembler {
         method.invoke(null, (Object) args);
     }
 
-    private String[] getMethods(String[] classes, String selectedClass, String pathComponent) throws MalformedURLException, ClassNotFoundException {
-        URL url = new File(pathComponent).toURL();
+    private String[] getMethods(String[] classes, String selectedClass, String dir) throws MalformedURLException, ClassNotFoundException {
+        URL url = new File(dir).toURL();
         URLClassLoader cl = new URLClassLoader(new URL[]{url});
         Class<?> c = cl.loadClass(classes[Integer.parseInt(selectedClass)]);
         Method[] methods = c.getDeclaredMethods();
         return Arrays.stream(methods).map(Method::getName).toArray(String[]::new);
     }
 
-    public Set<String> listJarFiles(String dir) {
+    private String[] getJarFiles(String dir) {
         return Stream.of(Objects.requireNonNull(new File(dir).listFiles()))
                 .filter(file -> !file.isDirectory() && file.getName().endsWith(".jar"))
                 .map(File::getName)
-                .collect(Collectors.toSet());
+                .toArray(String[]::new);
     }
 
-    public Set<String> getClassNamesFromJarFile(String dir) throws IOException, ClassNotFoundException {
+    private String[] getClassNamesFromJarFile(String dir) throws IOException {
         Set<String> set = new HashSet<>();
         JarFile jarFile = new JarFile(dir);
         Enumeration<JarEntry> e = jarFile.entries();
@@ -98,32 +122,24 @@ public class ComponentAssembler {
             String className = je.getName().substring(0, je.getName().length() - 6);
             set.add(className.replace('/', '.'));
         }
-        return set;
+        return set.toArray(new String[0]);
     }
 
-    public void run() throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        System.out.println("Start Component Assembler");
-        while (true) {
-            System.out.println("------------------------------------------");
-            String[] msg = {
-                    "show status",
-                    "run component",
-                    "stop component"
-            };
-            String input = ask("Please select number:", msg);
-            if (input.equals("exit")) {
-                break;
-            } else if (input.equals("1")) {
-                loadJar();
-            }
-        }
-    }
 
-    private String ask(String question, String[] options) {
+    private String ask(String question, String[] options, Breaker breaker) {
         System.out.println(question);
         for (int i = 0; i < options.length; i++) {
             System.out.println("[" + i + "] " + options[i]);
         }
+        if (breaker == Breaker.BACK) {
+            System.out.println("[" + options.length + "] " + Breaker.BACK.toString().toLowerCase(Locale.ROOT));
+        } else if (breaker == Breaker.EXIT) {
+            System.out.println("[" + options.length + "] " + Breaker.EXIT.toString().toLowerCase(Locale.ROOT));
+        }
         return sc.nextLine().toLowerCase();
+    }
+
+    enum Breaker {
+        EXIT, BACK, NONE
     }
 }
