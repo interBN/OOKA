0dc08dc9d3d6ae4f0a4c0a37b9992e6a823b0dba
Thorsten Laufenberg <thorsten@laufenberg.eu>
2022-05-04 20:57:32 +0200

    Very basic sendLog method Changed Inject from TYPE to FIELD

--- src/main/java/A3/Logger.java
+++ src/main/java/A3/Logger.java
@@ -1,10 +1,11 @@
 package A3;
 
+import java.time.LocalDateTime;
+
 public class Logger {
     public Logger(){
-
     }
     public void sendLog(String msg){
-
+        System.out.println("++++ LOG: " + msg + "(" + LocalDateTime.now() + ")");
     }
 }
