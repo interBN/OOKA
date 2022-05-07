a2757bdadec3e2d3c646dde704d06b958fecfe38
Siu Cheng <herrcheng@gmail.com>
2022-05-07 18:59:16 +0200

    :construction: update Timer

--- Tools/src/Timer.java
+++ Tools/src/Timer.java
@@ -1,10 +1,14 @@
 import A2.annotations.StartClassDeclaration;
 import A2.annotations.StartMethodDeclaration;
 import A2.annotations.StopMethodDeclaration;
+import A3.Inject;
+import A3.Logger;
 
 @StartClassDeclaration
 public class Timer {
 
+    @Inject
+    public Logger logger;
     boolean kill;
 
     public Timer() {
@@ -21,7 +25,12 @@ public class Timer {
         while (!kill) {
             try {
                 Thread.sleep(1000);
-                System.out.println("aiujsfdhiahf");
+//                System.out.println("aiujsfdhiahf");
+                if (logger != null) {
+                    logger.sendLog("alskifdjoiasdlfj");
+                } else {
+                    System.out.println("Injection does not work.");
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
